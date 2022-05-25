7c1297cf35123d2c8f0858961f133eb459280f8c
Rawia AbuRabie <aburabierawya@gmail.com>
2022-05-25 16:30:16 +0300

    rails new

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,27 @@
 class RestaurantsController < ApplicationController
-  def top
-    @restaurants = Restaurant.where(stars: 5)
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants do
-    resources :reviews, only: [ :new, :create ]
+    resources :reviews, only: [:new, :create ]
   end
 end
 
 Rails.application.routes.draw do
   resources :restaurants do
     member do
-      get :chef
+      get :index
     end
   end
+
 end
